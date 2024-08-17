ae6586041c132d2c66de571796e659261c95e5c8
Nicolaj Østerby Jensen <noje@cs.aau.dk>
2024-08-17 12:34:47 +0200

    Scale almost done

---
+++ scripts/Scale.gd
@@ -0,0 +1,50 @@
+extends Sprite2D
+
+@onready var armsNode: Sprite2D = $Arms
+@onready var leftNode: Area2D = $Arms/Left
+@onready var rightNode: Area2D = $Arms/Right
+@onready var leftNodeCupFoot: Node2D = $Arms/Left/CupFoot
+@onready var rightNodeCupFoot: Node2D = $Arms/Right/CupFoot
+
+@export var max_angle: float = 1
+@export var weight_for_max: int = 15
+@export var sigmoid_factor: float = 5
+
+@export var sum: int = 0
+var side_hovered: int = 0  # Left are sins
+
+func _process(delta: float) -> void:
+	var a = weight_to_angle(sum)
+	armsNode.rotation = a
+	leftNode.rotation = -a
+	rightNode.rotation = -a
+
+func weight_to_angle(w: int) -> float:
+	var f = 2.0 / (1.0 + exp(-sigmoid_factor * float(w) / weight_for_max)) - 1
+	return clamp(f * max_angle, -max_angle, max_angle)
+
+func _on_dropped(draggable: Draggable):
+	if side_hovered != 0:
+		sum += side_hovered * draggable.weight
+		draggable.being_weighed = true
+		var foot = rightNodeCupFoot
+		if side_hovered < 0:
+			foot = leftNodeCupFoot
+		draggable.reparent(foot)
+		draggable.position.y = 0
+
+func _on_pickup(draggable: Draggable):
+	sum -= side_hovered * draggable.weight   # BUG Small chance we are not hovering a side but still clicking the draggable
+	draggable.reparent(get_parent())
+
+func _on_left_mouse_entered() -> void:
+	side_hovered = -1
+
+func _on_left_mouse_exited() -> void:
+	side_hovered = max(0, side_hovered)
+
+func _on_right_mouse_entered() -> void:
+	side_hovered = 1
+
+func _on_right_mouse_exited() -> void:
+	side_hovered = min(0, side_hovered)
--- scripts/draggable.gd
+++ scripts/draggable.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
+class_name Draggable
+
+@export var can_be_weighed: bool = false
+@export var weight: int = 1
 
 var hovered: bool = false
 var dragging: bool = false
 var mouse_offset: Vector2
+var being_weighed: bool = false
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -11,9 +16,15 @@ func _input(event: InputEvent):
 			dragging = true
 			mouse_offset = position - get_viewport().get_mouse_position()
 			event.canceled = true
+			if being_weighed:
+				being_weighed = false
+				get_tree().root.get_child(0).find_child("Scale")._on_pickup(self)
 		elif event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
 			# Release anywhere
-			dragging = false
+			if dragging:
+				dragging = false
+				if can_be_weighed:
+					get_tree().root.get_child(0).find_child("Scale")._on_dropped(self)
 	elif event is InputEventMouseMotion:
 		# Mouse moved
 		if dragging:
