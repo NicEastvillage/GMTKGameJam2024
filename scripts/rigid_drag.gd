b16fcab5d7365fd02a061d5457898e904ba1e42c
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-08-17 20:03:30 +0200

    BING BONG

--- scripts/rigid_drag.gd
+++ scripts/rigid_drag.gd
@@ -8,16 +8,17 @@ var held = false
 var offset : Vector2
 var selected_sound: AudioStreamPlayer2D
 var dropped_sound: AudioStreamPlayer2D
+var collision_sound: AudioStreamPlayer2D
 var rng = RandomNumberGenerator.new()
 var pitch_variance = 0.5
 
 func _ready():
 	selected_sound = get_node_or_null("SelectedSound")
 	dropped_sound = get_node_or_null("DroppedSound")
+	collision_sound = get_node_or_null("CollisionSound")
 
 func play_sfx(sound):
 	if sound != null:
-		print("alive")
 		sound.pitch_scale = rng.randf_range(1 - pitch_variance, 1 + pitch_variance)
 		sound.playing = true
 
@@ -44,3 +45,7 @@ func drop(impulse=Vector2.ZERO):
 		#freeze = false
 		held = false
 		play_sfx(dropped_sound)
+
+
+func _on_body_entered(body: Node) -> void:
+	play_sfx(collision_sound)
