570d63db1e764e0c687c70f55e1de889a432e605
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-08-17 19:17:10 +0200

    SFX for draggables

--- scripts/draggable.gd
+++ scripts/draggable.gd
@@ -8,6 +8,14 @@ var hovered: bool = false
 var dragging: bool = false
 var mouse_offset: Vector2
 var being_weighed: bool = false
+var selected_sound: AudioStreamPlayer2D
+var dropped_sound: AudioStreamPlayer2D
+
+func _ready():
+	selected_sound = get_node_or_null("SelectedSound")
+	dropped_sound = get_node_or_null("DroppedSound")
+	
+
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -19,12 +27,16 @@ func _input(event: InputEvent):
 			if being_weighed:
 				being_weighed = false
 				get_tree().root.get_child(0).find_child("Scale")._on_pickup(self)
+			if selected_sound != null:
+				selected_sound.playing = true
 		elif event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
 			# Release anywhere
 			if dragging:
 				dragging = false
 				if can_be_weighed:
 					get_tree().root.get_child(0).find_child("Scale")._on_dropped(self)
+				if dropped_sound != null:
+					dropped_sound.playing = true
 	elif event is InputEventMouseMotion:
 		# Mouse moved
 		if dragging:
