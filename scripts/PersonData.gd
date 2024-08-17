a195ac38a4d800c448aafc8cb4d60d4a555fd162
Nicolaj Østerby Jensen <noje@cs.aau.dk>
2024-08-17 17:21:11 +0200

    Add Polaroids

--- scripts/PersonData.gd
+++ scripts/PersonData.gd
@@ -1,5 +1,7 @@
 extends Resource
 class_name PersonData
 
+@export var name: String = "N/A"
+@export var portrait: Texture2D = null
 @export var verdict_sinner: bool = false
 @export var person_documents: Array[PackedScene] = []
