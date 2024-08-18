1a186866afa37cd705ab4d0522249e2420b6d0a9
Thomas <thomasmgrosen@gmail.com>
2024-08-18 12:33:34 +0200

    add neutral margin to verdict

--- scripts/StageLoader.gd
+++ scripts/StageLoader.gd
@@ -102,14 +102,14 @@ func end_person(sinner: bool):
 		load_person()
 
 func give_verdict():
-	var sinner = scale_arms.rotation_degrees < 1.0  # Tiny bias
-	# TODO: Check if correct
-	if current_person.verdict_sinner == sinner:
-		print("VERDICT: sinner=", sinner, " (CORRECT)")
-	else:
-		print("VERDICT: sinner=", sinner, " (INCORRECT)")
-	
-	end_person(sinner)
+	if abs(scale_arms.rotation_degrees) > 2.0:
+		var sinner = scale_arms.rotation_degrees < 1.0  # Tiny bias
+		# TODO: Check if correct
+		if current_person.verdict_sinner == sinner:
+			print("VERDICT: sinner=", sinner, " (CORRECT)")
+		else:
+			print("VERDICT: sinner=", sinner, " (INCORRECT)")
+		end_person(sinner)
 
 var held_object = null
 
